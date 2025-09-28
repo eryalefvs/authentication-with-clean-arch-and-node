742773567afb701547c4ff85c92b21bdb78c13d4
eryalefvs <eryalef@gmail.com>
2025-09-27 23:03:13 -0300

    test: should return 200 on signup

--- tests/main/routes/login-routes.test.ts
+++ tests/main/routes/login-routes.test.ts
@@ -1,15 +1,48 @@
 import request from "supertest"
 import { setupApp } from "../../../src/main/config/app"
 import { Express } from "express"
+import { MongoHelper } from "@/infra/db/mongodb/mongo-helper"
+import { Collection } from "mongodb"
 
 let app: Express
+let accountCollection: Collection
     
-describe("SignUp Routes", () => {
-    test("Should return an account on success", async () => {
+describe("Login Routes", () => {
+    beforeAll(async () => {
         app = await setupApp()
-        await request(app)
-        .post("/api/signup")
-        .send({ name: "eryálef" })
-        .expect(200)
+        await MongoHelper.connect(process.env.MONGO_URL)
+    })
+
+    afterAll(async () => {
+        await MongoHelper.disconnect()
+    })
+
+    beforeEach(async () => {
+        accountCollection = MongoHelper.getCollection('accounts')
+        await accountCollection.deleteMany({})
+    })
+
+    describe("POST /signup", () => {
+        test("Should return 200 on signup", async () => {
+            await request(app)
+            .post("/api/signup")
+            .send({
+                name: "eryálef",
+                email: "ery@gmail.com",
+                password: "123",
+                passwordConfirmation: "123"
+            })
+            .expect(200)
+
+            await request(app)
+            .post("/api/signup")
+            .send({
+                name: "eryálef",
+                email: "ery@gmail.com",
+                password: "123",
+                passwordConfirmation: "123"
+            })
+            .expect(403)
+        })
     })
 })
