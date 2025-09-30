2f5fbbe409e3d337b49080682175e54b178b54b9
eryalefvs <eryalef@gmail.com>
2025-09-30 00:22:12 -0300

    feat: add helper unauthorized

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,4 +1,5 @@
 import { ServerError } from "../errors/server-error";
+import { UnauthorizedError } from "../errors/unauthorized-error";
 import { HttpResponse } from "../protocols/http";
 
 export const badRequest = (error: Error): HttpResponse => ({
@@ -19,4 +20,9 @@ export const ok = (data: any): HttpResponse => ({
 export const serverError = (error: Error): HttpResponse => ({
     statusCode: 500,
     body: new ServerError(error.stack)
+})
+
+export const unauthorized = (): HttpResponse => ({
+    statusCode: 401,
+    body: new UnauthorizedError()
 })
